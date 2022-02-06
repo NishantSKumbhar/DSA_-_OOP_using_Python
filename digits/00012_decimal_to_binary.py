9da530b73edbcf0f5d5f58237248e38c0ba48a40
Nishant <nish.kumbhar02@gmail.com>
2022-02-06 08:17:21 +0530

    Add files via upload

---
+++ digits/00012_decimal_to_binary.py
@@ -0,0 +1,9 @@
+"""
+@author  : Nishant Sanjay Kumbhar.
+@goal    : Convert Decimal to Binary.
+@github  : https://github.com/NishantSKumbhar
+@spotify : https://open.spotify.com/playlist/5xxjcOn9Dt8dhTCrN6S7Wb?si=e926b46a636a4d22
+"""
+
+a = 14
+print("Binary Form : {0:b}".format(a))
