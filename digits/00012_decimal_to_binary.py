ab7037bdb330dc79d2fc7cbfa6bc34e0c9c0df9c
Nishant <nish.kumbhar02@gmail.com>
2022-02-06 08:59:28 +0530

    Update 00012_decimal_to_binary.py

--- digits/00012_decimal_to_binary.py
+++ digits/00012_decimal_to_binary.py
@@ -4,6 +4,18 @@
 @github  : https://github.com/NishantSKumbhar
 @spotify : https://open.spotify.com/playlist/5xxjcOn9Dt8dhTCrN6S7Wb?si=e926b46a636a4d22
 """
-
+#  type 1
 a = 14
 print("Binary Form : {0:b}".format(a))
+
+# type 2
+
+def decimal_to_binary(num: int):
+    if num > 1:
+        decimal_to_binary(num // 2)
+    
+    print("Binary Formation : {}".format(num % 2))
+
+
+number = int(input("Enter the Number to Convert in Binary : "))
+decimal_to_binary(number)
